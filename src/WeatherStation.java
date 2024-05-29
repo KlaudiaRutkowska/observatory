39608dbf0f8e1d2787588daa1f17bdaf5280da72
Klaudia Rutkowska <rutkowskaklaudia731@gmail.com>
2024-05-29 21:57:55 +0200

    add exercise2

--- src/WeatherStation.java
+++ src/WeatherStation.java
@@ -3,10 +3,16 @@ public class WeatherStation {
         WeatherData weatherData = new WeatherData();
         new CurrentConditionsDisplay(weatherData);
         new WeatherForecast(weatherData);
+        new WeatherStatistics(weatherData);
 
         weatherData.setMeasurements(15, 36, 1004);
         weatherData.setMeasurements(18, 38, 1004);
-        weatherData.setMeasurements(15, 36, 1004);
+        weatherData.setMeasurements(24, 36, 1004);
         weatherData.setMeasurements(12, 32, 1004);
+        weatherData.setMeasurements(9, 32, 1004);
+        weatherData.setMeasurements(36, 32, 1004);
+        weatherData.setMeasurements(22, 32, 1004);
+        weatherData.setMeasurements(40, 32, 1004);
+        weatherData.setMeasurements(-4, 32, 1004);
     }
 }
--- src/WeatherStatistics.java
+++ src/WeatherStatistics.java
@@ -1,3 +1,34 @@
-public class WeatherStatistics {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class WeatherStatistics implements DisplayElement, Observer {
+    private List<Double> temps = new ArrayList<Double>();
+
+    public WeatherStatistics(Subject weatherData) {
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        double maxTemp = Collections.max(temps);
+        double minTemp = Collections.min(temps);
+        double avgTemp = 0;
+
+        for (Double temp : temps) {
+            avgTemp += temp;
+        }
+
+        avgTemp = avgTemp / temps.size();
+
+        System.out.println("Avg temp: " + avgTemp);
+        System.out.println("Max temp: " + maxTemp);
+        System.out.println("Min temp: " + minTemp);
+    }
+
+    @Override
+    public void update(double temperature, double humidity, double pressure) {
+        temps.add(temperature);
+        display();
+    }
 }
